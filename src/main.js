f843d04c5da9b1c5d32d07fba0a901f05e937d76
Clash2453 <spasmilenkov123@gmail.com>
2022-08-07 19:57:56 +0300

    Add the banner to the main page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {navigation} from './navigationBar';
+import { createBanner } from './banner';
 
 const content = document.createElement('div');
 content.id = 'content';
 const nav = navigation();
 content.appendChild(nav);
+content.appendChild(createBanner());
 
-document.body.appendChild(content)
+document.body.appendChild(content);
