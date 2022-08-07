385ac326774625f3c094d6a9f2a8566cd4e01c5a
Clash2453 <spasmilenkov123@gmail.com>
2022-08-07 19:57:18 +0300

    Add banner element to the main page

---
+++ src/banner.js
@@ -0,0 +1,15 @@
+const createBanner = () =>{
+    const mainBanner = document.createElement('div');
+    mainBanner.id = 'banner';
+    mainBanner.appendChild(createHeading());
+    return mainBanner;
+};
+
+const createHeading = () =>{
+    const heading = document.createElement('h1');
+    heading.id = 'main-header';
+    heading.textContent = 'Gourmet restaurant meals'
+    return heading;
+}
+
+export {createBanner};
