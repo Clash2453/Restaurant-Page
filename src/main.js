04505ef1f2bfecbb495153e8fcf9f68a8657d182
Clash2453 <spasmilenkov123@gmail.com>
2022-08-12 21:19:43 +0300

    Implement the promo section

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import {navigation} from './navigationBar';
 import { createBanner } from './banner';
+import { createSection } from './promoSection';
+import dessert from './resources/dessert.jpg';
+import delivery from './resources/delivery.jpg';
+import meat from './resources/steaks.jpg';
 
 const content = document.createElement('div');
 content.id = 'content';
 const nav = navigation();
 content.appendChild(nav);
 content.appendChild(createBanner());
+
+content.appendChild(createSection(meat,'#596F89', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+
+content.appendChild(createSection(delivery, '#6D8C9F',"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+
+content.appendChild(createSection(dessert, '#596F89',"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
 
 document.body.appendChild(content);
