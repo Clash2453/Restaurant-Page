16a1b56d8ba22e385530264dd7b2b9b77ce18e3d
Clash2453 <spasmilenkov123@gmail.com>
2022-08-13 20:04:14 +0300

    Add mirrored promo section creation function

--- src/promoSection.js
+++ src/promoSection.js
@@ -4,7 +4,7 @@ const createSection = (imagePath, background, text) => {
     const promoText = document.createElement('p');
 
     section.classList.add('promo-section');
-    section.style.backColor = background;
+    section.style.backgroundColor = background;
     picture.classList.add('promo-img');
     promoText.classList.add('promo-text');
     picture.src = imagePath;
@@ -16,4 +16,22 @@ const createSection = (imagePath, background, text) => {
     return section;
 }
 
-export {createSection};
+const createReverseSection = (imagePath, background, text) => {
+    const section = document.createElement('div');
+    const picture = document.createElement('img');
+    const promoText = document.createElement('p');
+
+    section.classList.add('promo-section');
+    section.style.backgroundColor = background;
+    picture.classList.add('promo-img');
+    promoText.classList.add('promo-text');
+    picture.src = imagePath;
+    promoText.textContent = text;
+
+    section.appendChild(promoText);
+    section.appendChild(picture);
+
+    return section;
+}
+
+export {createSection, createReverseSection};
