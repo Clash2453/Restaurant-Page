f8ce2f793758a251030dacb1bbde19a4c4b018b6
Clash2453 <spasmilenkov123@gmail.com>
2022-08-17 17:06:47 +0300

    Create the contacts page

--- src/contacts.js
+++ src/contacts.js
@@ -1,5 +1,25 @@
-const createContactsPage = () => {
+import { content } from "./main";
 
+const createContactsPage = () => {
+    const contactsContainer = document.createElement('div');
+    const card = document.createElement('div');
+    const contactsHeader = document.createElement('h1')
+    contactsHeader.id = 'main-header';
+    contactsHeader.textContent = 'Contacts'
+    contactsContainer.appendChild(contactsHeader);
+    card.classList.add('contacts-card');
+    contactsContainer.id = 'contacts-container';
+    contactsContainer.appendChild(card);
+    const email = document.createElement('h1');
+    const phoneNumber = document.createElement('h1');
+    email.classList.add('contacts-info');
+    phoneNumber.classList.add('contacts-info')
+    email.textContent = 'Email: johnDoe@example.com';
+    phoneNumber.textContent = 'Telephone for contacts: 0123456789';
+    card.appendChild(email);
+    card.appendChild(phoneNumber);
+
+    content.appendChild(contactsContainer);
 }
 
 export default createContactsPage
