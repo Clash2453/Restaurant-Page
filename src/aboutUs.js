80c758f1463b9651fd0a2cbaf379da1ab8c215be
Clash2453 <spasmilenkov123@gmail.com>
2022-08-17 13:08:27 +0300

    Add contact information to the page

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -1,12 +1,22 @@
 import { content } from "./main";
 const createAboutPage = () => {
     const aboutHeader = document.createElement('h1');
+    aboutHeader.id = 'about-header'
     const aboutContainer = document.createElement('div');
     const card = document.createElement('div');
+    const email = document.createElement('h1');
+    const phoneNumber = document.createElement('h1');
+    email.classList.add('info');
+    phoneNumber.classList.add('info')
+    email.textContent = 'Email: johnDoe@example.com';
+    phoneNumber.textContent = 'Telephone for contacts: 0123456789';
     card.classList.add('card');
+    card.appendChild(email);
+    card.appendChild(phoneNumber)
     aboutHeader.textContent = 'About us';
     aboutContainer.appendChild(aboutHeader);
     aboutContainer.id = 'about-container';
+    aboutContainer.appendChild(card);
     content.appendChild(aboutContainer);
 }
 
